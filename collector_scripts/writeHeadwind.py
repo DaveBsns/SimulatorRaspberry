d6a1e99a3dc4e037a3b03625645efdb337b994f6
unitylab-dev <unitylab.hhn3@gmail.com>
2023-12-18 18:17:10 +0100

    edit

--- collector_scripts/writeHeadwind.py
+++ collector_scripts/writeHeadwind.py
@@ -67,7 +67,7 @@ async def scan_and_connect():
     if(DEVICEID != ""):
         # Connecting to BLE Device
         # print("DEVICEID: ",DEVICEID.address)
-        async with BleakClient(DEVICEID, timeout=120) as client:
+        async with BleakClient(DEVICEID, timeout=60) as client:
             print("Device ID ", DEVICEID)
             for service in client.services:
                 # print("service: ", service)
@@ -87,8 +87,16 @@ async def scan_and_connect():
                             # print("CHARACTERISTIC: ", characteristic, characteristic.properties)
 
                             if ("write-without-response" in characteristic.properties):
-                                await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x01])) # Turns fan on -> bytearray([0x04, 0x04]), Turns fan off -> bytearray([0x04, 0x01]), Adjust fan Speed -> bytearray([0x02, <Decimalvalue between 1 and 100>])
-
+                                # while True:
+                                print("Type CHARACTERISTIC: ", type(CHARACTERISTIC))
+                                await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x04])) # Turns fan on -> bytearray([0x04, 0x04]), Turns fan off -> bytearray([0x04, 0x01]), Adjust fan Speed -> bytearray([0x02, <Decimalvalue between 1 and 100>])
+                                print("client connection: ", client.is_connected)
+                                # await client.disconnect()
+                                # print("client connection: ", client.is_connected)
+                                # await asyncio.sleep(10) # keeps the connection open for 10 seconds
+                                # print("TIMER")
+                                # await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, 100]))
+                                '''
                                 if ("notify" in characteristic.properties):
                                
                                     bluetooth_callback = BluetoothCallback()
@@ -99,6 +107,7 @@ async def scan_and_connect():
                                             # print("Die CHARARARA iSt: ", CHARACTERISTIC, CHARACTERISTIC.properties)
                                             # await client.start_notify(characteristic.uuid, on_notification)
                                             await client.start_notify(characteristic.uuid, bluetooth_callback.notify_callback)
+                                            print("client connection: ", client.is_connected)
                                             await asyncio.sleep(10) # keeps the connection open for 10 seconds
                                             await client.stop_notify(characteristic.uuid)                                     
                                             
@@ -106,10 +115,13 @@ async def scan_and_connect():
                                             print("Error: ", e)
                                             # run_read_loop = False
 
-
                                 '''
+                                
                                 # await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, 50])) # working if the fan is turned on and the script starting again
                                 while True:
+                                    print("client connection: ", client.is_connected)
+                                    await client.disconnect()
+                                    print("client connection: ", client.is_connected)
                                     speed_input = input("Enter a value between 1-100 to set the fan speed. Enter 0 to turn off the fan (or 'x' to exit): ")
                                     if speed_input.lower() == 'x':
                                         break
@@ -117,19 +129,95 @@ async def scan_and_connect():
                                         speed_value = int(speed_input)
                                         if 1 <= speed_value <= 100:
                                             # Convert speed value to byte and write to characteristic
-                                            await client.write_gatt_char(str(characteristic.uuid), bytearray([0x02]))
-                                            await client.write_gatt_char(str(characteristic.uuid), bytearray([0x07]))
+                                            await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, speed_value]))
                                             print(f"Fan speed set to {speed_value}")
                                         elif speed_value == 0:
-                                            await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x04]))
+                                            await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x01]))
                                         else:
                                             print("Speed value should be between 1 and 100.")
                                     except ValueError:
                                         print("Invalid input. Please enter a number between 1 and 100.")
                                 # bluetooth_callback = BluetoothCallback()
-                                '''
+                                
 
                                 
                                
 
 asyncio.run(scan_and_connect())
+
+
+async def connect_to_device(DEVICEID, SERVICE):
+    if(DEVICEID != ""):
+        # Connecting to BLE Device
+        # print("DEVICEID: ",DEVICEID.address)
+        async with BleakClient(DEVICEID, timeout=60) as client:
+            print("Device ID ", DEVICEID)
+            for service in client.services:
+                # print("service: ", service)
+                 
+                if (service.uuid == service_uuid):
+                        SERVICE = service
+                        # print("[service uuid] ", SERVICE.uuid)
+                            
+                if (SERVICE != ""):
+                    # print("SERVICE", SERVICE)
+                    for characteristic in SERVICE.characteristics:
+                        # if ("notify" in characteristic.properties):
+                        # print("[Characteristic] %s", characteristic, characteristic.properties)
+                        
+                        if(characteristic.uuid == characteristic_uuid):
+                            CHARACTERISTIC = characteristic
+                            # print("CHARACTERISTIC: ", characteristic, characteristic.properties)
+
+                            if ("write-without-response" in characteristic.properties):
+                                # while True:
+                                print("Type CHARACTERISTIC: ", type(CHARACTERISTIC))
+                                await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x04])) # Turns fan on -> bytearray([0x04, 0x04]), Turns fan off -> bytearray([0x04, 0x01]), Adjust fan Speed -> bytearray([0x02, <Decimalvalue between 1 and 100>])
+                                print("client connection: ", client.is_connected)
+                                # await client.disconnect()
+                                # print("client connection: ", client.is_connected)
+                                # await asyncio.sleep(10) # keeps the connection open for 10 seconds
+                                # print("TIMER")
+                                # await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, 100]))
+                                '''
+                                if ("notify" in characteristic.properties):
+                               
+                                    bluetooth_callback = BluetoothCallback()
+                                    while True:
+                                        try:
+                                            
+                                            # value = await client.read_gatt_char(characteristic.uuid)
+                                            # print("Die CHARARARA iSt: ", CHARACTERISTIC, CHARACTERISTIC.properties)
+                                            # await client.start_notify(characteristic.uuid, on_notification)
+                                            await client.start_notify(characteristic.uuid, bluetooth_callback.notify_callback)
+                                            print("client connection: ", client.is_connected)
+                                            await asyncio.sleep(10) # keeps the connection open for 10 seconds
+                                            await client.stop_notify(characteristic.uuid)                                     
+                                            
+                                        except Exception as e:
+                                            print("Error: ", e)
+                                            # run_read_loop = False
+
+                                '''
+                                
+                                # await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, 50])) # working if the fan is turned on and the script starting again
+                                while True:
+                                    print("client connection: ", client.is_connected)
+                                    await client.disconnect()
+                                    print("client connection: ", client.is_connected)
+                                    speed_input = input("Enter a value between 1-100 to set the fan speed. Enter 0 to turn off the fan (or 'x' to exit): ")
+                                    if speed_input.lower() == 'x':
+                                        break
+                                    try:
+                                        speed_value = int(speed_input)
+                                        if 1 <= speed_value <= 100:
+                                            # Convert speed value to byte and write to characteristic
+                                            await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, speed_value]))
+                                            print(f"Fan speed set to {speed_value}")
+                                        elif speed_value == 0:
+                                            await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x01]))
+                                        else:
+                                            print("Speed value should be between 1 and 100.")
+                                    except ValueError:
+                                        print("Invalid input. Please enter a number between 1 and 100.")
+                                # bluetooth_callback = BluetoothCallback()
