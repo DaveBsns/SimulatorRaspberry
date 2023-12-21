35efce6d9a06a158f37a5d0dcff7e9b25a2fcc41
David Flaig <david-flaig@gmx.de>
2023-12-21 16:55:05 +0100

    working on missing scripts

---
+++ collector_scripts/read_steering.py
@@ -0,0 +1,168 @@
+import asyncio
+from bleak import BleakScanner, BleakClient
+import struct
+
+class BluetoothCallback:
+    def __init__(self):
+        self.received_data = 0  # Initialize with None or any default value
+
+    async def notify_callback(self, sender, data):
+        # Assuming data is received from the Bluetooth device
+        if struct.pack("@h", 1) == struct.pack("<h", 1):
+            data = data[::-1]  # Reverse the byte order if little-endian
+
+        result = struct.unpack_from(">h", data, 2)[0]
+        output = result * 0.01
+        normalized_output = normalize(output, 0.0, 3.5)
+
+        if output < 0:
+            output = abs(output)
+        print(normalized_output)
+
+        self.received_data = normalized_output
+
+
+device_name = "RIZER"
+DEVICEID = ""
+
+service_uuid = "347b0001-7635-408b-8918-8ff3949ce592" # Rizer
+SERVICE = ""
+
+characteristic_uuid = "347b0030-7635-408b-8918-8ff3949ce592" # Rizer
+CHARACTERISTIC = ""
+
+value_to_write = ""
+old_value = ""
+
+run_read_loop = True
+is_first_entry = True
+
+def normalize(value, min_val, max_val):
+    range_val = max_val - min_val
+    normalized_value = (value - min_val) / range_val
+    return normalized_value
+
+
+async def on_notification(sender, data):
+    # collection = db["sensor_values"]  # Sammlungsname // outsourcen zur api
+    # data = data[0]
+    # value = data.decode('utf-8')
+
+    # Daten in die MongoDB schreiben
+    # entry = {"value": value}
+    # integer_value = int.from_bytes(data, byteorder='big') 
+    #collection.insert_one(entry)
+    # print(f"Data written to MongoDB: {entry}")
+    # print(f"Data written to MongoDB: {data}")
+    if struct.pack("@h", 1) == struct.pack("<h", 1):
+        data = data[::-1]  # Reverse the byte order if little-endian
+
+    result = struct.unpack_from(">h", data, 2)[0]
+    output = result * 0.01
+    normalized_output = normalize(output, 0.0, 3.5)
+
+    if output < 0:
+        output = abs(output)
+    print(normalized_output)
+
+    """
+    if (value[6] != value_to_write):
+        value_to_write = value[6]
+        if(entry_id == None):
+            write_in_db(value_to_write)
+            entry_id = await get_id()
+            #print("Entry ID: "+entry_id)
+            #message = value_to_write
+            print("Test", value_to_write)
+            # is_first_entry = False
+        else:
+            #message = value_to_write
+            print(value_to_write)
+            # await update_in_db(entry_id, value_to_write)
+            write_in_db(value_to_write)
+            logger.info("Updateing for id: %r ....With Value %r", entry_id, value_to_write)
+            #print("Updateing for id: ", entry_id, "....With Value ", value_to_write)
+            # print("old value: ", old_value)
+    
+        
+    old_value = value_to_write
+    logger.info(
+        "  [Characteristic] %s (%s), Value: %r",
+        characteristic,
+        ",".join(characteristic.properties) ,
+        # value[0],
+    )
+    """
+
+async def scan_and_connect():
+    global device_name
+
+    global service_uuid
+    global SERVICE
+
+    global characteristic_uuid
+    global CHARACTERISTIC
+
+    global value_to_write
+    global old_value
+
+    global is_first_entry
+    global run_read_loop  
+
+    stop_event = asyncio.Event()  
+
+    # Scanning and printing for BLE devices
+    def callback(device, advertising_data):
+        global DEVICEID   
+        print(device)
+        if(device.name == device_name):
+            DEVICEID = device
+            stop_event.set()
+            
+    # Stops the scanning event    
+    async with BleakScanner(callback) as scanner:
+        await stop_event.wait()
+    
+    if(DEVICEID != ""):
+        # Connecting to BLE Device
+        async with BleakClient(DEVICEID, timeout=120) as client:
+            # logger.info("Device ID ", device_id)
+            for service in client.services:
+                # print("service: ", service.properties)
+                
+                if (service.uuid == service_uuid):
+                        SERVICE = service
+                        print("[service uuid] ", SERVICE.uuid)
+                            
+                if (SERVICE != ""):
+                    # print("SERVICE", SERVICE)
+                    for characteristic in SERVICE.characteristics:
+                        # if ("notify" in characteristic.properties):
+                        print("[Characteristic] %s", characteristic, characteristic.properties)
+                        
+                        if(characteristic.uuid == characteristic_uuid):
+                            CHARACTERISTIC = characteristic
+                            print("CHARACTERISTIC: ", characteristic, characteristic.properties)
+                            if ("notify" in characteristic.properties):
+                                run_read_loop = True
+                                bluetooth_callback = BluetoothCallback()
+
+                                while run_read_loop:
+                                    try:
+                                        
+                                        # value = await client.read_gatt_char(characteristic.uuid)
+                                        # print("Die CHARARARA iSt: ", CHARACTERISTIC, CHARACTERISTIC.properties)
+                                        # await client.start_notify(characteristic.uuid, on_notification)
+                                        await client.start_notify(characteristic.uuid, bluetooth_callback.notify_callback)
+                                        await asyncio.sleep(10) # keeps the connection open for 10 seconds
+                                        await client.stop_notify(characteristic.uuid)                                     
+                                        
+                                    except Exception as e:
+                                        print("Error: ", e)
+                                        # run_read_loop = False                        
+                                   
+                                     
+asyncio.run(scan_and_connect())
+
+
+
---
+++ collector_scripts/write_height.py
@@ -0,0 +1,96 @@
+import asyncio
+from bleak import BleakScanner, BleakClient
+
+class BluetoothCallback:
+    def __init__(self):
+        self.received_data = 0  # Initialize with None or any default value
+
+    async def notify_callback(self, sender, data):
+        # Assuming data is received from the Bluetooth device
+        print(data)
+
+
+device_name = "RIZER"  # Replace with the name of your desired BLE device
+DEVICE = ""
+
+service_uuid = "00001800-0000-1000-8000-00805f9b34fb"
+SERVICE = ""
+
+characteristic_uuid = "00002a04-0000-1000-8000-00805f9b34fb" 
+CHARACTERISTIC = ""
+
+async def scan_and_connect():
+    global device_name
+
+    global service_uuid
+    global SERVICE
+
+    global characteristic_uuid
+    global CHARACTERISTIC
+
+    stop_event = asyncio.Event()  
+
+    # Scanning and printing for BLE devices
+    def callback(device, advertising_data):
+        global DEVICEID   
+        print(device)
+        if(device.name == device_name):
+            DEVICEID = device
+            stop_event.set()
+            
+    # Stops the scanning event    
+    async with BleakScanner(callback) as scanner:
+        await stop_event.wait()
+    
+    if(DEVICEID != ""):
+        # Connecting to BLE Device
+        async with BleakClient(DEVICEID, timeout=60) as client:
+            # print("Device ID ", DEVICEID)
+
+            for service in client.services:
+                print("service: ", service)
+                for characteristic in service.characteristics:
+                    print("Characteristics: ",characteristic)
+            '''
+            for service in client.services:
+                # print("service: ", service)
+                if (service.uuid == service_uuid):
+                    SERVICE = service
+
+                if (SERVICE != ""):
+                    for characteristic in SERVICE.characteristics:
+                        print(characteristic)
+                
+                    ###
+                        if(characteristic.uuid == characteristic_uuid):
+                            CHARACTERISTIC = characteristic
+
+                            bluetooth_callback = BluetoothCallback()
+                            while True:
+                                try:
+                                    await client.start_notify(CHARACTERISTIC, bluetooth_callback.notify_callback) # characteristic.uuid  
+                                except Exception as e:
+                                    print("Error: ", e)
+
+                                speed_input = input("Enter a value between 2-100 to set the fan speed. Enter 1 to turn on the fan. Enter 0 to turn off the fan (or 'x' to exit): ")
+                                speed_value = int(speed_input)
+                                if speed_input.lower() == 'x':
+                                    await client.stop_notify(CHARACTERISTIC) # characteristic.uuid
+                                    await asyncio.sleep(1)
+                                    break
+                                try:
+                                    if 2 <= speed_value <= 100:
+                                        await client.write_gatt_char(CHARACTERISTIC, bytearray([0x02, speed_value]))
+                                        print(f"Fan speed set to {speed_value}")
+                                    elif speed_value == 1:
+                                        await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x04])) # Turns fan on -> bytearray([0x04, 0x04]), Turns fan off -> bytearray([0x04, 0x01]), Adjust fan Speed -> bytearray([0x02, <Decimalvalue between 1 and 100>])
+                                    elif speed_value == 0:
+                                        await client.write_gatt_char(CHARACTERISTIC, bytearray([0x04, 0x01]))
+                                    else:
+                                        print("Speed value should be between 1 and 100.")
+                                except ValueError:
+                                    print("Invalid input. Please enter a number between 1 and 100.")
+                        '''
+                    
+
+asyncio.run(scan_and_connect())
