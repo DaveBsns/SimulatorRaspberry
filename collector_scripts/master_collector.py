b14144649c6456d0ba21b4f037c7723b92e236d8
unitylab-dev <unitylab.hhn3@gmail.com>
2024-02-13 16:13:35 +0100

    edit

--- collector_scripts/master_collector.py
+++ collector_scripts/master_collector.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
 
     # udp_unity_receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # udp_unity_receive_socket.bind((data_receiver.udp_unity_receive_ip, data_receiver.udp_unity_receive_port))
-
+    '''
     try:
         data_receiver.start_udp_listener()
         data_receiver.listen_for_udp_data()
@@ -128,80 +128,81 @@ if __name__ == "__main__":
     finally:
         pass
         # udp_unity_receive_socket.close()
+    '''
 
 
-        data_sender = DataSender()
-        # data_receiver = DataReceiver()
+    data_sender = DataSender()
+    # data_receiver = DataReceiver()
 
-        print("Master Collector script started...")
-        # IP adresses to receive data from actuators and sensors
-        # UDP_IP = "127.0.0.1" # IP of the computer running this script (just the localhost ip if the script is running on the same computer than the simulation)
-        # UDP_IP = "192.168.9.185" # IP of the computer running this script -> Bicycle Simulator Desktop PC
-        # UDP_ESP_IP = "192.168.9.185" # External IP of the computer running this script to receive data from ESP32 -> Bicycle Simulator Desktop PC
-        # UDP_IP = "192.168.9.184" # IP of the computer running this script -> Raspberry Pi
-        UDP_IP = "127.0.0.1" # IP to receive data from elite_rizer.py as well as from direto_xr.py scripts via UDP
-        # UDP_IP_DIRETO = "127.0.0.2"
-        UDP_ESP_IP = "192.168.9.184" # Raspberry Pi 3
-        # UDP_ESP_IP = "192.168.9.198" # Raspberry Pi 5
-
-
-        # ports to receive data from actuators and sensors
-        UDP_PORT_DIRETO = 1111
-        UDP_PORT_RIZER = 2222
-        UDP_PORT_ROLL = 6666
-        UDP_PORT_BRAKE = 7777
-        UDP_PORT_BNO = 8888
+    print("Master Collector script started...")
+    # IP adresses to receive data from actuators and sensors
+    # UDP_IP = "127.0.0.1" # IP of the computer running this script (just the localhost ip if the script is running on the same computer than the simulation)
+    # UDP_IP = "192.168.9.185" # IP of the computer running this script -> Bicycle Simulator Desktop PC
+    # UDP_ESP_IP = "192.168.9.185" # External IP of the computer running this script to receive data from ESP32 -> Bicycle Simulator Desktop PC
+    # UDP_IP = "192.168.9.184" # IP of the computer running this script -> Raspberry Pi
+    UDP_IP = "127.0.0.1" # IP to receive data from elite_rizer.py as well as from direto_xr.py scripts via UDP
+    # UDP_IP_DIRETO = "127.0.0.2"
+    UDP_ESP_IP = "192.168.9.184" # Raspberry Pi 3
+    # UDP_ESP_IP = "192.168.9.198" # Raspberry Pi 5
 
 
-        udp_rizer_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_rizer_socket.bind((UDP_IP, UDP_PORT_RIZER))
+    # ports to receive data from actuators and sensors
+    UDP_PORT_DIRETO = 1111
+    UDP_PORT_RIZER = 2222
+    UDP_PORT_ROLL = 6666
+    UDP_PORT_BRAKE = 7777
+    UDP_PORT_BNO = 8888
 
-        udp_direto_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_direto_socket.bind((UDP_IP, UDP_PORT_DIRETO))
 
-        udp_brake_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_brake_socket.bind((UDP_ESP_IP, UDP_PORT_BRAKE))
+    udp_rizer_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_rizer_socket.bind((UDP_IP, UDP_PORT_RIZER))
 
-        udp_bno_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_bno_socket.bind((UDP_ESP_IP, UDP_PORT_BNO))
+    udp_direto_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_direto_socket.bind((UDP_IP, UDP_PORT_DIRETO))
 
-        udp_roll_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_roll_socket.bind((UDP_ESP_IP, UDP_PORT_ROLL))
+    udp_brake_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_brake_socket.bind((UDP_ESP_IP, UDP_PORT_BRAKE))
 
+    udp_bno_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_bno_socket.bind((UDP_ESP_IP, UDP_PORT_BNO))
+
+    udp_roll_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_roll_socket.bind((UDP_ESP_IP, UDP_PORT_ROLL))
+
+    
+    
+    while True:
+        # print("udp_direto_socket: ", udp_direto_socket)
+        data_sender.send_unity_data_udp(data_sender.speed_value, data_sender.steering_value, data_sender.brake_value, data_sender.bno_value, data_sender.roll_value)
+        readable, _, _ = select.select([udp_rizer_socket, udp_direto_socket, udp_brake_socket, udp_bno_socket, udp_roll_socket], [], [])
         
-        
-        while True:
-            # print("udp_direto_socket: ", udp_direto_socket)
-            data_sender.send_unity_data_udp(data_sender.speed_value, data_sender.steering_value, data_sender.brake_value, data_sender.bno_value, data_sender.roll_value)
-            readable, _, _ = select.select([udp_rizer_socket, udp_direto_socket, udp_brake_socket, udp_bno_socket, udp_roll_socket], [], [])
-            
-            for sock in readable:
-                data, addr = sock.recvfrom(1024)
-                if sock is udp_rizer_socket:
-                    #print(f"Received data on port {UDP_RIZER}: {data.decode()} from {addr}")
-                    steering_value = data.decode()
-                    # print("steering: ", steering_value)
-                    data_sender.collect_steering(steering_value)
-                elif sock is udp_direto_socket:
-                    #print(f"Received data on port {UDP_DIRETO}: {data.decode()} from {addr}")
-                    speed_value = data.decode()
-                    print("SPEED: ", speed_value)
-                    data_sender.collect_speed(speed_value)
-                elif sock is udp_brake_socket:
-                    #print(f"Received data on port {UDP_BRAKE}: {data.decode()} from {addr}")
-                    brake_value = json.loads(data.decode())
-                    brake_value = brake_value["sensor_value"]
-                    # print("Brake_value: ", brake_value)
-                    data_sender.collect_brake(brake_value)
-                elif sock is udp_bno_socket:
-                    #print(f"Received data on port {UDP_BNO}: {data.decode()} from {addr}")
-                    bno_value = json.loads(data.decode())
-                    bno_value = bno_value["euler_r"]
-                    # print("BNO_Value: ", bno_value)
-                    data_sender.collect_bno(bno_value)
-                elif sock is udp_roll_socket:
-                    #print(f"Received data on port {UDP_BNO}: {data.decode()} from {addr}")
-                    roll_value = json.loads(data.decode())
-                    # print("Roll_Value: ", roll_value)
-                    roll_value = roll_value["sensor_value"]
-                    data_sender.collect_roll(roll_value)         
+        for sock in readable:
+            data, addr = sock.recvfrom(1024)
+            if sock is udp_rizer_socket:
+                #print(f"Received data on port {UDP_RIZER}: {data.decode()} from {addr}")
+                steering_value = data.decode()
+                # print("steering: ", steering_value)
+                data_sender.collect_steering(steering_value)
+            elif sock is udp_direto_socket:
+                #print(f"Received data on port {UDP_DIRETO}: {data.decode()} from {addr}")
+                speed_value = data.decode()
+                print("SPEED: ", speed_value)
+                data_sender.collect_speed(speed_value)
+            elif sock is udp_brake_socket:
+                #print(f"Received data on port {UDP_BRAKE}: {data.decode()} from {addr}")
+                brake_value = json.loads(data.decode())
+                brake_value = brake_value["sensor_value"]
+                # print("Brake_value: ", brake_value)
+                data_sender.collect_brake(brake_value)
+            elif sock is udp_bno_socket:
+                #print(f"Received data on port {UDP_BNO}: {data.decode()} from {addr}")
+                bno_value = json.loads(data.decode())
+                bno_value = bno_value["euler_r"]
+                # print("BNO_Value: ", bno_value)
+                data_sender.collect_bno(bno_value)
+            elif sock is udp_roll_socket:
+                #print(f"Received data on port {UDP_BNO}: {data.decode()} from {addr}")
+                roll_value = json.loads(data.decode())
+                # print("Roll_Value: ", roll_value)
+                roll_value = roll_value["sensor_value"]
+                data_sender.collect_roll(roll_value)         
