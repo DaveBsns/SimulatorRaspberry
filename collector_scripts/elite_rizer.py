464755af2410c668f57fe539528c0de5fb7e294c
Philipp <philipp.senfft@gmail.com>
2024-08-28 20:01:40 +0200

    deleted unnecessary code

--- collector_scripts/elite_rizer.py
+++ collector_scripts/elite_rizer.py
@@ -2,174 +2,227 @@ import asyncio
 from bleak import BleakClient, exc
 import socket
 import time
-from master_collector import DataReceiver
+import json
 
-DEVICE_NAME = "RIZER"       
-DEVICE_UUID = "fc:12:65:28:cb:44"
+class Rizer:
+    #BLE constant
+    DEVICE_NAME = "RIZER"       
+    DEVICE_UUID = "fc:12:65:28:cb:44"
 
-SERVICE_STEERING_UUID = "347b0001-7635-408b-8918-8ff3949ce592" # Rizer - read steering
-SERVICE_TILT_UUID = "347b0001-7635-408b-8918-8ff3949ce592"
+    SERVICE_STEERING_UUID = "347b0001-7635-408b-8918-8ff3949ce592"           # Rizer - read steering
+    SERVICE_INCLINE_UUID = "347b0001-7635-408b-8918-8ff3949ce592"
 
-INCREASE_TILT_HEX = "060102"
-DECREASE_TILT_HEX = "060402"
+    INCREASE_INCLINE_HEX = "060102"
+    DECREASE_INCLINE_HEX = "060402"
 
-steering_service = ""
-tilt_service = ""
+    CHARACTERISTICS_STEERING_UUID = "347b0030-7635-408b-8918-8ff3949ce592"   # Rizer - read steering
+    CHARACTERISTIC_INCLINE_UUID = "347b0020-7635-408b-8918-8ff3949ce592"     # write tilt
 
-stering_ready = 0
-tilt_ready = 0
+    #UDP constant
+    UDP_IP_TO_MASTER_COLLECTOR = "127.0.0.1"        # Send the rizer data to the master_collector.py script via UDP over localhost
+    UDP_IP_FROM_MASTER_COLLECTOR = "127.0.0.3"
+    udp_port = 2222
+    RECEIVE_FROM_MASTER_COLLECTOR_PORT = 2223
 
-stored_tilt_value = 0
-
-CHARACTERISTICS_STEERING_UUID = "347b0030-7635-408b-8918-8ff3949ce592" # Rizer - read steering
-CHARACTERISTIC_TILT_UUID = "347b0020-7635-408b-8918-8ff3949ce592" # write tilt
-
-
-steering_characteristics = ""
-tilt_characteristics = ""
+    _incline_value = 0
+    incline_rate = 1
+    incline_timer = 0
 
 
-class BluetoothCallback:
-    def __init__(self):
-        self.received_steering_data = 0  # Initialize with None or any default value
+
+    def __init__(self) -> None:
+        # UDP initialization
+        self.received_steering_data = 0             # Initialize with None or any default value
+        self.client_is_connected = False
+
+
+        self.current_incline_on_rizer = int(0)
         self.udp_ip = "127.0.0.1" # Send the rizer data to the master_collector.py script via UDP over localhost
         # self.udp_ip = "10.30.77.221" # Ip of the Bicycle Simulator Desktop PC
         # self.udp_ip = "192.168.9.184" # IP of the Raspberry Pi
         self.udp_port = 2222
-        
+        print("init")
 
-    async def notify_steering_callback(self, sender, data):
-        data = bytearray(data)
-        steering = 0.0
-        
-        if data[3] == 65:
-            steering = 1.0
-        elif data[3] == 193:
-            steering = -1.0
-        
-        self.received_steering_data = steering
-        print(self.received_steering_data)
+    async def main(self):
+        print("main")
+        incline_value = 0
 
-        self.send_steering_data_udp(self.received_steering_data)
+        # create UDP socket to receive data from master collector
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+            udp_socket.bind((self.UDP_IP_FROM_MASTER_COLLECTOR, self.RECEIVE_FROM_MASTER_COLLECTOR_PORT))
+            udp_socket.setblocking(False)                                                                   # Set the socket to non-blocking mode | without this flag it waits until it gets data
+            await self.connect_rizer()                                                                      #connect to the Rizer                                                                      
+            while(True):
+                # Receive data from the socket
+                try:
+                    udp_incline_data = 0
+                    while True:
+                        try:
+                            udp_incline_data, addr = udp_socket.recvfrom(47)
+                            sender_ip, sender_port = addr
+                            print(f"Received message: {udp_incline_data.decode()} from {sender_ip}:{sender_port}")
+                            incline_value = json.loads(udp_incline_data.decode())
+                            incline_value = int(incline_value["rizerIncline"])                                                 # Extract the sender's IP and port from addr
+                        except BlockingIOError:
+                            break
+                                                           
 
+                except BlockingIOError:
+                    time.sleep(0.01)  # Small sleep to prevent busy-waiting
+                
+
+                await self.write_incline()                                                          #write the new incline value to the Rizer
+
+                self.set_incline_value(incline_value)
+                if self.check_new_incline(incline_value):                                               #check if the incline value has changed
+                    await self.write_incline()                                                          #write the new incline value to the Rizer
+
+                await self.read_steering()                                                              #read steering over BLE ONLY WORKING with master_collector script!!!
+                
+
+#---------------------------UDP functions--------------------------------
     #send steering data over udp
     def send_steering_data_udp(self, steering_data):
-        # Create a UDP socket
-        # print(steering_data)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-            # Send speed_data
-            udp_socket.sendto(str(steering_data).encode(), (self.udp_ip, self.udp_port))
+            # Send steering_data
+            udp_socket.sendto(str(steering_data).encode(), (self.UDP_IP_TO_MASTER_COLLECTOR, self.udp_port))
 
-    def listening_udp(self):
+    #receive incline data over udp
+    async def receive_incline_data_udp(self):
+        udp_incline_data = 0
+        # Set up the UDP socket
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-            udp_socket.listen(str(udp_tilt_data).encode(), (self.udp_ip, self.udp_port))
-            print("Hello: ", udp_tilt_data)
+            # Bind the socket to the IP and port
+            udp_socket.bind((self.UDP_IP_TO_MASTER_COLLECTOR, self.RECEIVE_FROM_MASTER_COLLECTOR_PORT))
+            print("Hello: ", udp_incline_data)
+            self.incline_value = udp_incline_data
 
-async def read_steering(client, characteristic):
-    bluetooth_callback = BluetoothCallback()
-    try:
-        await client.start_notify(characteristic, bluetooth_callback.notify_steering_callback)
-        await asyncio.sleep(10) # keeps the connection open for 10 seconds
-        await client.stop_notify(characteristic.uuid) 
-        # print("Test steering")                                    
-    except Exception as e:
-        print("Error: ", e)                    
 
-async def write_tilt(client, characteristic):
-    bluetooth_callback = BluetoothCallback()
-    try:
-        #TODO Write bt stuff
-        print("BT stuff")
-        await client.write_gatt_char(CHARACTERISTIC_TILT_UUID, bytes.fromhex(INCREASE_TILT_HEX), response=True)
-        stored_tilt_value += 0.5
-    except Exception as e:
-        print("Error: ", e) 
 
-# check if the value of the tilt in unity is the same as on the rizer (currently not possible to check the value. just to store the changes)
-def get_new_tilt_value():
-    receiver = DataReceiver()
-    try:
-        receiver.start_udp_listener()
-        tilt_value = receiver.get_tilt()
-        print("RIZER tilt: ", tilt_value)
-        if tilt_value != stored_tilt_value:
-            stored_tilt_value = tilt_value
+#---------------------------BLE functions--------------------------------
+    #function to initialize the BLE connection with the Rizer
+    async def connect_rizer(self):
+        print("start async init")
+        while not self.client_is_connected:
+            try:
+                self.client = BleakClient(self.DEVICE_UUID, timeout=90)
+                print("try to connect")
+                await self.client.connect()
+                self.client_is_connected = True
+                print("Client connected to ", self.DEVICE_UUID)
+                for service in self.client.services:
+                    if (service.uuid == self.SERVICE_STEERING_UUID):
+                        self.steering_service = service
+                        print("[service uuid] ", self.steering_service.uuid)
 
-    except Exception as e:
-        print("Error: ", e)
-           
-
-async def scan_and_connect_rizer():
-    global DEVICE_NAME
-
-    global SERVICE_STEERING_UUID
-
-    global steering_service
-    global tilt_service
-
-    global CHARACTERISTICS_STEERING_UUID
-    global steering_characteristics
-    global tilt_characteristics
-
-    global stering_ready                    #connection to steering BLE service ready
-    global tilt_ready                       #connection to tilt BLE service ready
-    
-    # Connecting to BLE Device
-    client_is_connected = False
-    while(client_is_connected == False):
-        try:
-            async with BleakClient(DEVICE_UUID, timeout=90) as client:
-                client_is_connected = True
-                #print("Client connected to ", DEVICE_ID.name)
-                #print("Client connected to ", DEVICE_ID)
-                print("Client connected to ", DEVICE_UUID)
-                # return True
-                # logger.info("Device ID ", device_id)
-                for service in client.services:
-                    # print("service: ", service)
-                    
-                    if (service.uuid == SERVICE_STEERING_UUID):
-                        steering_service = service
-                        print("[service uuid] ", steering_service.uuid)
-
-                        if (steering_service != ""):
+                        if (self.steering_service != ""):
                             # print("SERVICE", SERVICE)
-                            for characteristic in steering_service.characteristics:
+                            for characteristic in self.steering_service.characteristics:
                                 
-                                if("notify" in characteristic.properties and characteristic.uuid == CHARACTERISTICS_STEERING_UUID):
-                                    steering_characteristics = characteristic
+                                if("notify" in characteristic.properties and characteristic.uuid == self.CHARACTERISTICS_STEERING_UUID):
+                                    self.steering_characteristics = characteristic
                                     # print("CHARACTERISTIC: ", CHARACTERISTIC_STEERING, characteristic.properties)
-                                print("IF")
 
-                        print(stering_ready)
-                        stering_ready = 1 
+                        #print("steering ready", self.steering_ready)
+                        self.steering_ready = 1 
 
-                    if (service.uuid == SERVICE_TILT_UUID):
-                        tilt_service = service
-                        print("[service uuid] ", tilt_service.uuid)
+                    if (service.uuid == self.SERVICE_INCLINE_UUID):
+                        self.incline_service = service
+                        print("[service uuid] ", self.incline_service.uuid)
 
-                        if (tilt_service != ""):
-                            for characteristic in tilt_service.characteristics:
+                        if (self.incline_service != ""):
+                            for characteristic in self.incline_service.characteristics:
 
                                 print("characteristics UUID: ", characteristic.uuid)
-                                if("notify" in characteristic.properties and characteristic.uuid == CHARACTERISTIC_TILT_UUID):
-                                    tilt_characteristics = characteristic
-                        print(tilt_ready)
-                        tilt_ready = 1
+                                if("notify" in characteristic.properties and characteristic.uuid == self.CHARACTERISTIC_INCLINE_UUID):
+                                    self.incline_characteristics = characteristic
+                        self.incline_ready = 1
 
-                while (stering_ready == 1 and tilt_ready == 1):
+                if (self.steering_ready == 1 and self.incline_ready == 1):
                     print("all ready!")
-                    await read_steering(client, steering_characteristics)
-                    await write_tilt(client, tilt_characteristics)
-                    print(tilt_characteristics)
-                    print("read and write!")
- 
+                    self.init_ack = True
 
-        except exc.BleakError as e:
-            print(f"Failed to connect/discover services of {DEVICE_UUID}: {e}")
-            # Add additional error handling or logging as needed
-            # raise 
+            except exc.BleakError as e:
+                print(f"Failed to connect/discover services of {self.DEVICE_UUID}: {e}")
+                # Add additional error handling or logging as needed
+                # raise
 
-asyncio.run(scan_and_connect_rizer())
+    #write incline to rizer over ble and store the value of his state
+    async def write_incline(self):
+        incline = self.get_incline_value()
+        incline = int(incline)
+        #print("current incline: ", int(self.current_incline_on_rizer), " Incline: ", int(incline) )
+        #incline_different_temp = int(incline) - int(self.current_incline_on_rizer)         #absolute difference of old and new incline value
+        #incline_different = abs(incline_different_temp)
+        #print("incline_difference: ", incline_different)
+        # print("Incline: ", incline, " current Incline: ", current_incline_on_rizer)
 
-'create UDP_Handler in own task'
+        if((int(incline) - int(self.current_incline_on_rizer)) > 0):
+            #for x in range (incline_different):
+                try:
+                    await self.client.write_gatt_char(self.CHARACTERISTIC_INCLINE_UUID, bytes.fromhex(self.INCREASE_INCLINE_HEX), response=True)
+                    self.current_incline_on_rizer += 1
+                    #self.incline_received = 0
+                    #print("incline written, x ", x)
+                except Exception as e:
+                    print("Error: ", e) 
+        else:
+            #for x in range (incline_different):
+                try:
+                    await self.client.write_gatt_char(self.CHARACTERISTIC_INCLINE_UUID, bytes.fromhex(self.DECREASE_INCLINE_HEX), response=True)
+                    self.current_incline_on_rizer += -1
+                    #self.incline_received = 0
+                    #print("incline written, x -", x)
+                except Exception as e:
+                    print("Error: ", e)
+
+    #read steering from the Rizer
+    async def read_steering(self):
+        data = bytearray(8)
+        sender = 0
+        try:
+            await self.client.start_notify(self.steering_characteristics, self.notify_steering_callback)
+            # Access the notification data using data argument
+            # print(f"Steering data: {sender}")
+            # print(f"Steering data: {data}")
+            await asyncio.sleep(0.1) # keeps the connection open for 10 seconds
+            await self.client.stop_notify(self.steering_characteristics.uuid)                                  
+        except Exception as e:
+            print("Error: ", e) 
+
+
+
+#---------------------------Utility functions--------------------------------
+    def set_incline_value(self, incline):
+        incline = min(int(incline), 40)
+        incline = max(int(incline), -20)
+        self._incline_value = incline
+
+    def get_incline_value(self):
+        return self._incline_value
+    
+    async def notify_steering_callback(self, sender, data):
+            data = bytearray(data)
+            steering = 0.0
+            
+            #we get this random values who stands for -1; 0 or 1
+            if data[3] == 65:
+                steering = 1.0
+            elif data[3] == 193:
+                steering = -1.0
+            
+            self.received_steering_data = steering
+            print(self.received_steering_data)
+
+            self.send_steering_data_udp(self.received_steering_data)
+
+    #check if rizer alredy arrived position
+    def check_new_incline(self, new_inline_udp):
+        if self.current_incline_on_rizer != new_inline_udp:
+            print("incline value write BLE: ", self.get_incline_value())
+            return True
+        else:
+            return False      
+
+rizer = Rizer()
+asyncio.run(rizer.main())
