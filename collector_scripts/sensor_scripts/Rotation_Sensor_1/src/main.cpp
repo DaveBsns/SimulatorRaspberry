f02475e5dd4be45fa35cf286f940987394bcb2cb
Mirko Schuerlein <mirkoschuerlein@googlemail.com>
2024-12-02 18:46:12 +0100

    added speed calculation

--- collector_scripts/sensor_scripts/Rotation_Sensor_1/src/main.cpp
+++ collector_scripts/sensor_scripts/Rotation_Sensor_1/src/main.cpp
@@ -3,20 +3,39 @@
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
 
-// SENSOR
-const int HALL_PIN = 32;
-
 // WIFI
 const char *ssid = "Bicycle_Simulator_Network";
 const char *password = "17701266";
 unsigned int localUdpPort = 7778;
-
 WiFiUDP udp;
+
+// SENSOR
+const int HALL_PIN = 32;
+const int UDP_DELAY_MS = 20;
+const int N_MAGNETS = 4;
+const int AVG_SIZE = 50;  // Sliding average window size
+const int AVG_DELAY = UDP_DELAY_MS * AVG_SIZE;
+
+// TODO: deal with integer overflows
+struct rotationMetrics {
+  int count = 0;
+  int counts[AVG_SIZE] = {0};
+  float speed = 0;
+  float acceleration = 0;
+};
+rotationMetrics metrics;
+
+void IRAM_ATTR isr() {
+  metrics.count++;
+}
 
 void setup() {
   // WIFI
   WiFi.hostname("Rotation_ESP");
   WiFi.begin(ssid, password);
+
+  pinMode(HALL_PIN, INPUT_PULLUP);
+  attachInterrupt(HALL_PIN, isr, FALLING);
 
   Serial.begin(115200);
 
@@ -31,17 +50,19 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//   Serial.println("LED is on");
-//   delay(1000);
-//   Serial.println("LED is off");
-//   delay(1000);
-
+  // Calculate speed
+  for (int k=AVG_SIZE-1; k>0; k--) {
+    metrics.counts[k] = metrics.counts[k-1];
+  }
+  metrics.counts[0] = metrics.count;
+  metrics.speed = (1000.0 * (metrics.counts[0] - metrics.counts[AVG_SIZE-1])) / float(AVG_DELAY * N_MAGNETS);
+  
+  // Build JSON message
   StaticJsonDocument<500> doc;
-
   String jsonStr;
   doc["sensor"] = "Rotation";
-  doc["sensor_value"] = 5;
+  doc["sensor_value"] = metrics.speed;
+  doc["count"] = metrics.count;
   serializeJson(doc, jsonStr);
 
   // Read UDP messages
@@ -54,11 +75,12 @@ void loop() {
     Serial.println(packetBuffer);
   }
 
+  // Send UDP message
   udp.beginPacket("192.168.0.101", localUdpPort);
   udp.print(jsonStr);
   udp.endPacket();
 
   Serial.println(jsonStr);
 
-  delay(2000);
+  delay(UDP_DELAY_MS);
 }
